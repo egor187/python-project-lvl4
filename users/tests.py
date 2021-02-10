5c462aaac0080d33df75c46fd1d608b3eeaf90b5
Егор <oegor8194@gmail.com>
2021-02-10 21:46:28 +0300

    add test_delete_object

--- users/tests.py
+++ users/tests.py
@@ -3,11 +3,11 @@ from django.urls import reverse
 from .models import CustomUser
 
 
-class CreateUserViewTest(TestCase):
+class UserViewTest(TestCase):
     def setUp(self):
         credentials = {'username': 'John', 'password': 'aswdqwerty5'}
-        credentials2 = {'username': 'Bob', 'password': 'aswdqwerty5'}
-        user = CustomUser.objects.create_user(**credentials)
+        # credentials2 = {'username': 'Bob', 'password': 'aswdqwerty5'}
+        CustomUser.objects.create_user(**credentials)
         credentials_login = {'username': 'John', 'password': 'aswdqwerty5'}
         self.client.login(**credentials_login)
 
@@ -30,7 +30,23 @@ class CreateUserViewTest(TestCase):
         self.assertContains(response_after_update, CustomUser.objects.get(username='Bob'))
 
     def test_delete_user(self):
-        pass
+        user_pk = CustomUser.objects.get(username='John').pk
+        url = f'/users/{user_pk}/delete/>'
+        response_from_delete_form = self.client.get(url)
+        self.assertEqual(response_from_delete_form.status_code, 200)
+        self.assertEqual(1, len(CustomUser.objects.all()))
+        self.client.delete(url, {'username': 'John'})
 
-    def test_incorrect_form_input(self):
-        pass
+        response_after_delete = self.client.get(reverse('users:user_list'))
+        self.assertEqual(response_after_delete.status_code, 200)
+        self.assertNotEqual(1, CustomUser.objects.all())
+
+    def test_create_user_within_form_(self):
+        url = '/users/create/'
+        response_from_create_form = self.client.get(url)
+        self.assertEqual(response_from_create_form.status_code, 200)
+        self.client.post(url, {'username': 'David', 'password1': 'level5level5', 'password2': 'level5level5'})
+
+        response_after_update = self.client.get(reverse('users:user_list'))
+        self.assertEqual(response_after_update.status_code, 200)
+        self.assertContains(response_after_update, CustomUser.objects.get(username='David'))
