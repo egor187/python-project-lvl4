b8d455a8c7b2f818d00908b6f45d419784b1450c
Егор <oegor8194@gmail.com>
2021-02-12 22:02:46 +0300

    add tasks.ListStatusesView and template

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from tasks.models import Task, TaskStatus
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
-# Create your views here.
+
+class ListStatusesView(generic.ListView):
+    model = TaskStatus
+    template_name = 'tasks_index.html'
+
+
+
