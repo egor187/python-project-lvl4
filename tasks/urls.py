d4e5d4ce0b24beedfc1632648f4a54b91ca51719
Егор <oegor8194@gmail.com>
2021-02-13 15:36:09 +0300

    add "create" tu statuses

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,7 +6,7 @@ app_name = 'tasks'
 
 urlpatterns = [
     path('statuses/', views.ListStatusesView.as_view(), name='statuses_list'),
-#    path('statuses/create/', views.CreateStatusView.as_view(), name='create_status'),
+    path('statuses/create/', views.CreateStatusView.as_view(), name='create_status'),
 #    path('statuses/<int:pk>/update/', views.UpdateStatusView.as_view(), name='update_status'),
 #    path('statuses/<int:pk>/delete/', views.DeleteStatusView.as_view(), name='felete_status'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from tasks.models import Task, TaskStatus
 from django.urls import reverse_lazy
 from django.views import generic
+from django.views.generic import edit
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext as _
 
 
 class ListStatusesView(generic.ListView):
@@ -11,4 +13,11 @@ class ListStatusesView(generic.ListView):
     template_name = 'statuses_index.html'
 
 
-
+class CreateStatusView(LoginRequiredMixin, SuccessMessageMixin, edit.CreateView):
+    model = TaskStatus
+    fields = [
+        'name',
+    ]
+    template_name = 'status_create_form.html'
+    success_url = reverse_lazy('tasks:statuses_list')
+    success_message = _('%(name)s-status was successfully created')
