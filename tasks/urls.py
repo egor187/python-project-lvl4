8fd6549d5480525c9c6993a44531aa7814b01aac
Егор <oegor8194@gmail.com>
2021-02-13 17:26:25 +0300

    add statuses:update and statuses:delete

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,7 +6,9 @@ app_name = 'tasks'
 
 urlpatterns = [
     path('statuses/', views.ListStatusesView.as_view(), name='statuses_list'),
+    path('statuses/<int:pk>/', views.StatusView.as_view(template_name='status_view.html'), name='status'),
+    path('statuses/<int:pk>/update/', views.UpdateStatusView.as_view(), name='update_status'),
+    path('statuses/<int:pk>/delete/', views.DeleteStatusView.as_view(), name='delete_status'),
     path('statuses/create/', views.CreateStatusView.as_view(), name='create_status'),
-#    path('statuses/<int:pk>/update/', views.UpdateStatusView.as_view(), name='update_status'),
-#    path('statuses/<int:pk>/delete/', views.DeleteStatusView.as_view(), name='felete_status'),
+
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from tasks.models import Task, TaskStatus
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib import messages
 from django.views.generic import edit
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
@@ -13,6 +14,10 @@ class ListStatusesView(generic.ListView):
     template_name = 'statuses_index.html'
 
 
+class StatusView(generic.DetailView):
+    model = TaskStatus
+
+
 class CreateStatusView(LoginRequiredMixin, SuccessMessageMixin, edit.CreateView):
     model = TaskStatus
     fields = [
@@ -20,4 +25,36 @@ class CreateStatusView(LoginRequiredMixin, SuccessMessageMixin, edit.CreateView)
     ]
     template_name = 'status_create_form.html'
     success_url = reverse_lazy('tasks:statuses_list')
-    success_message = _('%(name)s-status was successfully created')
+    success_message = _('"%(name)s"-status was successfully created')
+
+
+class UpdateStatusView(LoginRequiredMixin, SuccessMessageMixin, edit.UpdateView):
+    model = TaskStatus
+    fields = [
+        'name',
+    ]
+    template_name = 'status_update_form.html'
+    success_url = reverse_lazy('tasks:statuses_list')
+    success_message = _('"%(name)s"-status was successfully updated')
+
+
+class DeleteStatusView(LoginRequiredMixin, SuccessMessageMixin, edit.DeleteView):
+    model = TaskStatus
+    fields = [
+        'name',
+    ]
+    template_name = 'status_delete_form.html'
+    success_url = reverse_lazy('tasks:statuses_list')
+    success_message = _('status was successfully deleted')
+
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            result = super().delete(request, *args, **kwargs)
+            if self.success_message:
+                messages.success(request, self.success_message)
+            return result
+        except ProtectedError:
+            if self.protected_message:
+                messages.error(request, self.protected_message)
+            return redirect(self.protected_url)
