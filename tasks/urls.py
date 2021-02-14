1c2afe7530a1677f1f3d29e8c2e957839d303abc
Егор <oegor8194@gmail.com>
2021-02-14 22:38:07 +0300

    add endpoints (class-based views) for tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,8 +7,13 @@ app_name = 'tasks'
 urlpatterns = [
     path('statuses/', views.ListStatusesView.as_view(), name='statuses_list'),
     path('statuses/<int:pk>/', views.StatusView.as_view(template_name='status_view.html'), name='status'),
+    path('statuses/create/', views.CreateStatusView.as_view(), name='create_status'),
     path('statuses/<int:pk>/update/', views.UpdateStatusView.as_view(), name='update_status'),
     path('statuses/<int:pk>/delete/', views.DeleteStatusView.as_view(), name='delete_status'),
-    path('statuses/create/', views.CreateStatusView.as_view(), name='create_status'),
 
+    path('tasks/', views.ListTasksView.as_view(), name='tasks_list'),
+    path('tasks/<int:pk>', views.TaskView.as_view(), name='task'),
+    path('tasks/create/', views.CreateTaskView.as_view(), name='create_task'),
+    path('tasks/<int:pk>/update/', views.UpdateTaskView.as_view(), name='update_task'),
+    path('tasks/<int:pk>/delete/', views.DeleteTaskView.as_view(), name='delete_task'),
 ]
