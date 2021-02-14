9a7875acdfc8bea4f3ee223607ed997a77c6530c
Егор <oegor8194@gmail.com>
2021-02-14 16:31:37 +0300

    add reassigned method "get_context_data" for access to "session attribute of self.request object" for count numbers of connections from each client to server and get variable for tamplate

--- task_manager/views.py
+++ task_manager/views.py
@@ -4,8 +4,23 @@ from django.views.generic.base import TemplateView
 
 
 def index(request):
-    return render(request, 'index.html')
+    car = request.session['my_car'] = 'bmw'
+    visits = request.session.get('visits', 0)
+    request.session['visits'] = visits + 1
+    return render(request, 'index.html', context ={'car':car, 'visits': visits})
 
 
 class Index(TemplateView):
     template_name = 'index.html'
+
+    # Reassigned super method to get context_var - "num_visits" with number of connections from session.
+    def get_context_data(self, **kwargs):
+        # Access to sessions in class-based views available from request object, which constructs by .as_view() in
+        # self.request. That's why access to self.request in class-based views available also in others method of
+        # class-based views, not only from get_context_data
+        visits = self.request.session.get('visits', 0)
+        self.request.session['visits'] = visits + 1
+        num_visits = self.request.session['visits'] = visits + 1
+        context = super().get_context_data(**kwargs)
+        context['num_visits'] = num_visits
+        return context
