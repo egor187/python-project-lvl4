ed86ab105be25024726c3a08aa1135ada62ce386
Егор <oegor8194@gmail.com>
2021-02-14 10:29:53 +0300

    add ordering bi username in CustomUser model by add inner class Meta

--- users/models.py
+++ users/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
 
+    class Meta():
+        ordering = ['username']
+
     def __str__(self):
         return self.username
 
