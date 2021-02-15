2a4971fa34a1bf7f6c0f56b698120e6e40f922a9
Егор <oegor8194@gmail.com>
2021-02-15 22:22:28 +0300

    change routes to tasks: delete "/tasks/" in route

--- tasks/urls.py
+++ tasks/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('statuses/<int:pk>/update/', views.UpdateStatusView.as_view(), name='update_status'),
     path('statuses/<int:pk>/delete/', views.DeleteStatusView.as_view(), name='delete_status'),
 
-    path('tasks/', views.ListTasksView.as_view(), name='tasks_list'),
-    path('tasks/<int:pk>', views.TaskView.as_view(), name='task'),
-    path('tasks/create/', views.CreateTaskView.as_view(), name='create_task'),
-    path('tasks/<int:pk>/update/', views.UpdateTaskView.as_view(), name='update_task'),
-    path('tasks/<int:pk>/delete/', views.DeleteTaskView.as_view(), name='delete_task'),
+    path('', views.ListTasksView.as_view(), name='tasks_list'),
+    path('<int:pk>', views.TaskView.as_view(), name='task'),
+    path('create/', views.CreateTaskView.as_view(), name='create_task'),
+    path('<int:pk>/update/', views.UpdateTaskView.as_view(), name='update_task'),
+    path('<int:pk>/delete/', views.DeleteTaskView.as_view(), name='delete_task'),
 ]
