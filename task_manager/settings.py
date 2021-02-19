de2f459a75ed898765bfc89f57664d7802f5079b
Егор <oegor8194@gmail.com>
2021-02-19 12:23:11 +0300

    change language settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -144,7 +144,7 @@ LOGOUT_REDIRECT_URL = 'home'
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 # List of acceptable languages for app  for i18n purpose
 LANGUAGES = [
