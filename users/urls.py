c556e770813980a985c6f03e53e3aa4b4a7b0ba8
Егор <oegor8194@gmail.com>
2021-02-20 21:21:10 +0300

    fix null object.pk for "delete" link of object

--- users/urls.py
+++ users/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('<int:pk>/', views.UserView.as_view(template_name='user_view.html'), name='user'),
     path('create/', views.CreateUserView.as_view(template_name='user_create_form.html'), name='create_user'),
     path('<int:pk>/update/', views.UpdateUserView.as_view(template_name='user_update.html'), name='update_user'),
-    path('<int:pk>/delete/>', views.DeleteUserView.as_view(template_name='user_delete.html'), name='delete_user'),
+    path('<int:pk>/delete/', views.DeleteUserView.as_view(template_name='user_delete.html'), name='delete_user'),
     # path('login/', views.LoginUserView.as_view(template_name='login.html'), name='login'),
     # path('logout/', views.LogoutUserView.as_view(template_name='login.html'), name='logout'),
 
