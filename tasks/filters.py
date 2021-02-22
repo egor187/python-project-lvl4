d477804c6c54d0dc63eea9821d0248c70c693174
Егор <oegor8194@gmail.com>
2021-02-22 12:12:57 +0300

    refactor filter: add "label"-kwarg which change name of a field in filter

--- tasks/filters.py
+++ tasks/filters.py
@@ -1,8 +1,14 @@
 import django_filters
-from tasks.models import Task
+from tasks.models import Task, Label
+
+# class TaskFilter(django_filters.FilterSet):
+#     new = django_filters.CharFilter(field_name='label', label='Метка')
+#     class Meta:
+#         model = Task
+#         fields = ['task_status', 'assigned_to', 'label', 'creator', 'new']
 
 class TaskFilter(django_filters.FilterSet):
-
+    new_name = django_filters.ModelMultipleChoiceFilter(queryset=Label.objects.all(), field_name='label', label='Метка')
     class Meta:
         model = Task
-        fields = ['task_status', 'assigned_to', 'label', 'creator']
+        fields = ['task_status', 'assigned_to', 'creator', 'new_name']
