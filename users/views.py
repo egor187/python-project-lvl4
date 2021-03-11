80869200d85c73d6b2cceba0c898d8df8ac0b0f3
Егор <oegor8194@gmail.com>
2021-03-11 22:03:58 +0300

    set DEBUG=False

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic.edit import FormView,\
